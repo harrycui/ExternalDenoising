eb4ad09d58ed40a8bafd0b11bb3ab4ef0a44c84a
harrycui <cuihelei@gmail.com>
2015-11-19 09:57:42 +0800

    Beta version.
    This version is used in our paper.

--- TIFSDenoising/src/test/TestgroundtruthOpt.java
+++ TIFSDenoising/src/test/TestgroundtruthOpt.java
@@ -3,10 +3,8 @@ package test;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
